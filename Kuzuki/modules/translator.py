718f8630b1592f49efd9085ac0feda2bcf985f85
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 00:39:34 +0530

    Update translator.py

--- Kuzuki/modules/translator.py
+++ Kuzuki/modules/translator.py
@@ -11,8 +11,8 @@ from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from AnieRobot import dispatcher, LOGGER
-from AnieRobot.modules.disable import DisableAbleCommandHandler
+from Kuzuki import dispatcher, LOGGER
+from Kuzuki.modules.disable import DisableAbleCommandHandler
 
 from googletrans import Translator
 
