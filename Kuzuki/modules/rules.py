23ad2f5c18fc3842b8e0796f1c8857dfdcd03be2
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 02:13:33 +0530

    Update rules.py

--- Kuzuki/modules/rules.py
+++ Kuzuki/modules/rules.py
@@ -13,10 +13,10 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown
 
-import AnieRobot.modules.sql.rules_sql as sql
-from AnieRobot import dispatcher
-from AnieRobot.modules.helper_funcs.chat_status import user_admin, user_can_change
-from AnieRobot.modules.helper_funcs.string_handling import markdown_parser
+import Kuzuki.modules.sql.rules_sql as sql
+from Kuzuki import dispatcher
+from Kuzuki.modules.helper_funcs.chat_status import user_admin, user_can_change
+from Kuzuki.modules.helper_funcs.string_handling import markdown_parser
 
 
 
