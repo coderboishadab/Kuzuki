601363da327cde7522185fcd91fb4221334501a1
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 02:16:16 +0530

    Update remote_cmds.py

--- Kuzuki/modules/remote_cmds.py
+++ Kuzuki/modules/remote_cmds.py
@@ -13,12 +13,12 @@ from telegram.error import BadRequest
 from telegram.ext import run_async, CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
-from AnieRobot import dispatcher
-from AnieRobot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_ban_protected, can_restrict, \
+from Kuzuki import dispatcher
+from Kuzuki.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_ban_protected, can_restrict, \
     is_user_admin, is_user_in_chat, is_bot_admin
-from AnieRobot.modules.helper_funcs.extraction import extract_user_and_text
-from AnieRobot.modules.helper_funcs.string_handling import extract_time
-from AnieRobot.modules.helper_funcs.filters import CustomFilters
+from Kuzuki.modules.helper_funcs.extraction import extract_user_and_text
+from Kuzuki.modules.helper_funcs.string_handling import extract_time
+from Kuzuki.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
