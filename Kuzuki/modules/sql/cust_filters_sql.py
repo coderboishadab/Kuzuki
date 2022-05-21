e71982d0793a7c17e5ecef51b7f214d499abeff0
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 20:26:17 +0530

    Update cust_filters_sql.py

--- Kuzuki/modules/sql/cust_filters_sql.py
+++ Kuzuki/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from AnieRobot.modules.helper_funcs.msg_types import Types
-from AnieRobot.modules.sql import BASE, SESSION
+from Kuzuki.modules.helper_funcs.msg_types import Types
+from Kuzuki.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
