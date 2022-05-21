29a3c54e7efec3e72e0709413e09c5c15facf9d9
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 20:25:42 +0530

    Update connection_sql.py

--- Kuzuki/modules/sql/connection_sql.py
+++ Kuzuki/modules/sql/connection_sql.py
@@ -3,8 +3,8 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer, func, distinct
 
-from AnieRobot.modules.helper_funcs.msg_types import Types
-from AnieRobot.modules.sql import SESSION, BASE
+from Kuzuki.modules.helper_funcs.msg_types import Types
+from Kuzuki.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
