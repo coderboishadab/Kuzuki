d2a624c5b7ac07e8724663bb8632bf601c586ddb
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 20:24:37 +0530

    Update blacklistusers_sql.py

--- Kuzuki/modules/sql/blacklistusers_sql.py
+++ Kuzuki/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from AnieRobot.modules.sql import BASE, SESSION
+from Kuzuki.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
