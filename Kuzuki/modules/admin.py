e01341c6f59c1287e96b8f48004e25538a0d6bbb
zeusop5 <99333164+zeusop5@users.noreply.github.com>
2022-05-21 09:46:07 +0530

    Update admin.py

--- Kuzuki/modules/admin.py
+++ Kuzuki/modules/admin.py
@@ -1,31 +1,3 @@
-"""
-MIT License
-
-Copyright (C) 2017-2019, Paul Larsen
-Copyright (C) 2021 Noobanon 
-Copyright (c) 2021, Denvil, <https://github.com/Anieteam/AnieRobot>
-
-This file is part of @Anierobot_bot (Telegram Bot)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import os
 import html
 
@@ -40,9 +12,9 @@ from telethon.errors import *
 from telethon.tl import *
 from telethon import *
 
-from AnieRobot import SUDO_USERS, dispatcher, telethn as bot
-from AnieRobot.modules.disable import DisableAbleCommandHandler
-from AnieRobot.modules.helper_funcs.chat_status import (
+from Kuzuki import SUDO_USERS, dispatcher, telethn as bot
+from Kuzuki.modules.disable import DisableAbleCommandHandler
+from Kuzuki.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -54,12 +26,12 @@ from AnieRobot.modules.helper_funcs.chat_status import (
     can_manage_voice_chats,
 )
 
-from AnieRobot.modules.helper_funcs.extraction import (
+from Kuzuki.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from AnieRobot.modules.log_channel import loggable
-from AnieRobot.modules.helper_funcs.alternate import send_message
+from Kuzuki.modules.log_channel import loggable
+from Kuzuki.modules.helper_funcs.alternate import send_message
 
 
 @user_admin
